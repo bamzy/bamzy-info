822600008588ef36057abf7f2c9cf7053bc5a27f
Bamdad <bamdad@mortgageautomator.com>
2023-02-22 16:22:17 -0500

    added two more analyzer

--- apis/chart/chartAPI.js
+++ apis/chart/chartAPI.js
@@ -12,10 +12,11 @@ server.use(express.json());
 server.use(express.urlencoded());
 server.use(cors());
 
-const analyze = (url,res)=>{
+const scrape = (url,res)=>{
     axios.get(url).then((response)=>{
         res.header('Content-Type','text/html');
         let data = response.data;
+
         var result = data.match(/<description>(.*?)<\/description>/g).map(function(val){
             val = val.replace(/<\/?description>/g,'');
             val = val.replace('![CDATA[','');
@@ -38,7 +39,10 @@ const analyze = (url,res)=>{
 
             for (let j = 0; j < arr.length; j++) {
                 let currentWord = arr[j];
-                if(excludedWords.has(currentWord)) continue;
+                if(excludedWords.has(currentWord)) {
+                    // console.log("this word was found:"+ currentWord);
+                    continue;
+                }
                 if(currentWord.length<=3) continue;
                 if (words.get(currentWord)) words.set(currentWord,words.get(currentWord)+1);
                 else words.set(currentWord,1);
@@ -48,7 +52,7 @@ const analyze = (url,res)=>{
         const sortedWords = new Map([...words.entries()].sort((a, b) => b[1] - a[1]));
         let finalArr = [...sortedWords.entries()];
         let chartResult = [];
-        for(let i=0;i<finalArr.length&&i<30;i++){
+        for(let i=0;i<finalArr.length&&i<75;i++){
             chartResult.push({x:finalArr[i][0],value:finalArr[i][1]})
         }
         res.send(chartResult);
@@ -56,10 +60,16 @@ const analyze = (url,res)=>{
 }
 server.get('/analyzeBBC',(req,res)=>{
 
-    analyze('https://feeds.bbci.co.uk/persian/rss.xml',res);
+    scrape('https://feeds.bbci.co.uk/persian/rss.xml',res);
 })
 server.get('/analyzeIranIntl',(req,res)=>{
-    analyze('https://www.iranintl.com/feed',res);
+    scrape('https://www.iranintl.com/feed',res);
+})
+server.get('/analyzeRadioFarda',(req,res)=>{
+    scrape('https://www.radiofarda.com/api/zrttpoeuoupo',res);
+})
+server.get('/analyzeTasnim',(req,res)=>{
+    scrape('https://www.tasnimnews.com/fa/rss/feed/0/8/0/%D9%85%D9%87%D9%85%D8%AA%D8%B1%DB%8C%D9%86-%D8%A7%D8%AE%D8%A8%D8%A7%D8%B1',res);
 })
 server.listen(port,()=>{
     console.log(`Chat Server running on port ${port}`)
--- apis/chart/wordRepo.js
+++ apis/chart/wordRepo.js
@@ -2,6 +2,13 @@ const getExcludedWords = ()=>{
     let map = new Map();
     map.set('است');
     map.set('برای');
+    map.set('ایران');
+    map.set('است.');
+    map.set('دیگر');
+    map.set('جمهوری اسلامی');
+    map.set('امروز');
+    map.set('کرده');
+    map.set('کرد');
     return map;
 }
 module.exports = { getExcludedWords}
