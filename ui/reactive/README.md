4f62171ff888b60defc383713eef7a8c941ed336
Bamdad <bamdad@mortgageautomator.com>
2023-03-02 17:34:49 -0500

    added missing files to repo

---
+++ ui/reactive/README.md
@@ -0,0 +1,33 @@
+# [Iran News Monitor ️](https://horizon-ui.com/horizon-ui-chakra) 
+
+![version](https://img.shields.io/badge/version-0.0.1-brightgreen.svg)
+![license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+### Quick Start
+
+Install Iran News Monitor by running either of the following:
+
+- Install NodeJS on your server from [NodeJs Official Page](https://nodejs.org/en/)
+
+Clone the repository with the following command:
+
+```bash
+git clone https://github.com/bamzy/bamzy-info.git
+```
+
+navigate to the address below:
+
+```bash
+cd ui/reactive
+```
+Run in terminal this command:
+
+```bash
+npm install
+```
+
+Then run this command to start your local server
+
+```bash
+npm start
+```
---
+++ ui/reactive/src/routes.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+import { Icon } from "@chakra-ui/react";
+import {MdBarChart, MdPerson, MdHome, MdLock} from "react-icons/md";
+import {GiNewspaper} from 'react-icons/gi'
+import {RiGalleryFill} from 'react-icons/ri'
+// Admin Imports
+import MainDashboard from "views/admin/default";
+import GraphGallery from "views/admin/graphGallery";
+import Profile from "views/admin/profile";
+import DataTables from "views/admin/dataTables";
+import RTL from "views/admin/rtl";
+
+// Auth Imports
+import SignInCentered from "views/auth/signIn";
+const sidebarIconSize = '30px';
+const routes = [
+  {
+    name: "Main Dashboard",
+    layout: "/admin",
+    path: "/default",
+    icon: <Icon as={GiNewspaper} width={sidebarIconSize} height={sidebarIconSize} color='inherit' />,
+    component: MainDashboard,
+  },
+  {
+    name: "Graph Gallery",
+    layout: "/admin",
+    path: "/graph-gallery",
+    icon: <Icon as={RiGalleryFill} width={sidebarIconSize} height={sidebarIconSize} color='inherit' />,
+    component: GraphGallery,
+    secondary: true,
+  },
+  {
+    name: "Data Tables",
+    layout: "/admin",
+    icon: <Icon as={MdBarChart} width={sidebarIconSize} height={sidebarIconSize} color='inherit' />,
+    path: "/data-tables",
+    component: DataTables,
+  },
+  {
+    name: "Profile",
+    layout: "/admin",
+    path: "/profile",
+    icon: <Icon as={MdPerson} width={sidebarIconSize} height={sidebarIconSize} color='inherit' />,
+    component: Profile,
+  },
+  {
+    name: "Sign In",
+    layout: "/auth",
+    path: "/sign-in",
+    icon: <Icon as={MdLock} width={sidebarIconSize} height={sidebarIconSize} color='inherit' />,
+    component: SignInCentered,
+  },
+  {
+    name: "RTL Admin",
+    layout: "/rtl",
+    path: "/rtl-default",
+    icon: <Icon as={MdHome} width={sidebarIconSize} height={sidebarIconSize} color='inherit' />,
+    component: RTL,
+  },
+];
+
+export default routes;
