9d03438bc45dc535cdd8ea7345915c86e4fef6cc
Bamdad <bamdad@mortgageautomator.com>
2023-03-06 17:58:37 -0500

    a working code analysis finally

--- ui/reactive/src/views/admin/profile/components/CodeAnalysis.js
+++ ui/reactive/src/views/admin/profile/components/CodeAnalysis.js
@@ -1,5 +1,5 @@
 // Chakra imports
-import {Box, Flex, Icon, Progress, Text, Badge, useColorModeValue,} from "@chakra-ui/react";
+import {Stat,StatLabel,StatNumber,StatHelpText,Spinner,Box, Flex, Icon, Progress, Text, Badge,Tooltip, useColorModeValue,} from "@chakra-ui/react";
 // Custom components
 import Card from "components/card/Card.js";
 import IconBox from "components/icons/IconBox";
@@ -9,13 +9,15 @@ import React, {useState} from "react";
 // Assets
 import { MdQueryStats } from "react-icons/md";
 import axios from "axios";
+import {parse} from "stylis";
 
 export default class CodeAnalysis extends React.Component{
 
     constructor(props){
         super(props);
-        this.state = {badges:[]};
+        this.state = {badges:<Spinner />};
 
+        this.colors = [ "gray" , "red" , "orange" , "yellow" , "green" , "teal" , "blue" , "cyan" , "purple" , "pink" , "#1dce52" , "#ce1da9" , "messenger" , "whatsapp" , "twitter" , "telegram"]
 
     }
     componentDidMount() {
@@ -24,13 +26,24 @@ export default class CodeAnalysis extends React.Component{
 
     }
     loadBadges(){
-        console.log("url:"+this.props.url)
         axios.get(Constants.codestatsAPIUrl).then((resp)=>{
-            debugger;
-            let badges = resp.data.map((item)=>{
-                return (<Badge>{item['Language']}</Badge>);
+            // debugger;
+            let badges = resp.data.items.map((item,index)=>{
+                // return (<Tooltip label={item['Language']}>line of code: {item['code']}</Tooltip>);
+                // let index = parseInt(Math.random()*this.colors.length);
+                let color = this.colors[index];
+                // if
+                console.log(index);
+                if(item['code']==0) return null;
+                return (
+                    <Stat bg={color} color='black' style={{borderRadius:'25%',fontSize:'7px',padding:'5px'}} >
+                        <StatLabel >{item['Language']}</StatLabel>
+                        <StatNumber style={{fontSize:'10px'}}>{item['code']}</StatNumber>
+                    </Stat>
+                )
             });
-            // this.setState({badges:badges})
+            // debugger;
+            this.setState({badges:badges})
         });
     }
     // textColorPrimary = useColorModeValue("secondaryGray.900", "white");
@@ -51,21 +64,23 @@ export default class CodeAnalysis extends React.Component{
                 h='100px'
                 w='100px'
                 icon={
-                  <Icon as={MdQueryStats} color={this.brandColor} h='46px' w='46px' />
+                  <Icon as={MdQueryStats} color={"brand.500"} h='46px' w='46px' />
                 }
                 bg={"secondaryGray.300"}
               />
               <Text color={"secondaryGray.900"} fontWeight='bold' fontSize='2xl' mt='10px'>
-                Project Analysis
+                    Repo Breakdown
               </Text>
-                <Flex>
-                   analyzing...
-                </Flex>
+                <Box class="p-3">
+                    <Flex>
+                    {this.state.badges}
+                    </Flex>
+                </Box>
               <Text
                 color={"secondaryGray.600"}
                 fontSize='md'
                 maxW={{ base: "100%", xl: "80%", "3xl": "60%" }}
-                mx='auto'>
+                mx='auto' className='mt-2'>
                 total amount of data stored in the system
               </Text>
               <Box w='100%' mt='auto'>
