f59dd74d378e090913074cb35459bd6301683f27
Bamdad <bamdad@mortgageautomator.com>
2023-04-23 17:50:03 -0400

    error page

--- ui/demo/src/pages/ErrorPage.js
+++ ui/demo/src/pages/ErrorPage.js
@@ -3,32 +3,40 @@ import { styled } from '@mui/material/styles';
 import {Error} from '@mui/icons-material';
 import {Link,Grid,Paper} from "@mui/material";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
 
 const ErrorPage = (props) => {
     return (
         <Grid container spacing={1} mt={10}>
             <Grid xs={12} justifyContent={"center"} alignItems={"center"}>
-                <Item>
+                {/* <Item> */}
                 <Typography  align="center" >
                     <Error   color="error" sx={{fontSize:100}} />
                 </Typography>
 
-                </Item>
+                {/* </Item> */}
             </Grid>
             <Grid xs={12}>
-                <Item>
-                <Typography variant="h1" mt={0} color="text.secondary" align="center" >
-                    Error {props.code} happened
+                {/* <Item> */}
+                <Typography variant="h2" mt={0} color="text.secondary" align="center" >
+                    Error {props.code} happened! 
                 </Typography>
 
-                </Item>
+                {/* </Item> */}
+            </Grid>
+            <Grid xs={12}>
+                {/* <Item> */}
+                <Typography variant="h4" mt={0} color="text.secondary" align="center" >
+                     please try again later
+                </Typography>
+
+                {/* </Item> */}
             </Grid>
             
         </Grid>
