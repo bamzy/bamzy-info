68613c6c83ed7075601feb0fd2a5b9cf28468148
Bamdad <bamdad@mortgageautomator.com>
2023-04-23 17:47:24 -0400

    we have an acceptable error page, let's move on

--- ui/demo/src/pages/ErrorPage.js
+++ ui/demo/src/pages/ErrorPage.js
@@ -1,5 +1,39 @@
-const ErrorPage = () => {
-    return <div>you ran into error or sth</div>
+import  Typography  from "@mui/material/Typography";
+import { styled } from '@mui/material/styles';
+import {Error} from '@mui/icons-material';
+import {Link,Grid,Paper} from "@mui/material";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const ErrorPage = (props) => {
+    return (
+        <Grid container spacing={1} mt={10}>
+            <Grid xs={12} justifyContent={"center"} alignItems={"center"}>
+                <Item>
+                <Typography  align="center" >
+                    <Error   color="error" sx={{fontSize:100}} />
+                </Typography>
+
+                </Item>
+            </Grid>
+            <Grid xs={12}>
+                <Item>
+                <Typography variant="h1" mt={0} color="text.secondary" align="center" >
+                    Error {props.code} happened
+                </Typography>
+
+                </Item>
+            </Grid>
+            
+        </Grid>
+        
+  )
 }
 
 export default ErrorPage;
